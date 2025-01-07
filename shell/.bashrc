6e35760fdb5db35f36c066fc3924cdf152053d7e
Nofal Briansah <nofalbriansah@gmail.com>
2025-01-07 08:21:24 +0700

    init

---
+++ shell/.bashrc
@@ -0,0 +1,23 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+# my PATH
+if [ -f ~/.bash_paths ]; then
+    . ~/.bash_paths
+fi
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+fi
+unset rc
---
+++ shell/.config/fish/conf.d/abbr.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+   # terminal
+   abbr --add dot "cd /home/nbs/dotfiles"
+   abbr --add ff fastfetch
+   abbr --add du "sudo dnf update && sudo dnf upgrade"
+   abbr --add fu "flatpak update"
+   abbr --add cl clear
+   abbr --add treed "tree -d -a -L 5"
+   abbr --add edit gnome-text-editor
+   abbr --add vim nvim
+	
+   # dev
+   abbr --add hg "bunx blowfish-tools"
+   abbr --add fld "flutter doctor -v"
+   abbr --add cdev "cd /mnt/Data/development/coding"
+   abbr --add cebook "cd /mnt/Data/development/research/ebooks"
+end
