b47afb0bd50badd5301b02ff067a2f433c0b15ad
Nofal Briansah <48385577+nofalbriansah@users.noreply.github.com>
2025-01-09 03:49:20 +0700

    Update config.fish

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -8,7 +8,7 @@ set -x PATH $PATH $JAVA_HOME/bin
 
 set -x ANDROID_HOME /home/nbs/dev/Android/Sdk
 set -x PATH $PATH $ANDROID_HOME/platform-tools
-# Flutter SDK flutter config --jdk-dir
+# flutter config --jdk-dir
 set -x PATH $PATH /home/nbs/dev/flutter/bin
 
 set -x PATH $PATH /home/nbs/dev/bun/bin
